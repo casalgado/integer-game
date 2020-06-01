6dde443b3f41c7347657876413b5aacb484ba664
casalgado <casalgado86@gmail.com>
2020-06-01 16:35:54 -0500

    first players added

---
+++ src/components/game/Player.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    :id="id"
+    class="circle"
+    draggable
+    @ondrag.prevent
+    @dragstart="startDrag($event, id)"
+  >
+    <svg width="50" height="50">
+      <circle
+        cx="25"
+        cy="25"
+        r="24"
+        stroke="black"
+        stroke-width="1"
+        fill="white"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Player",
+  props: {
+    id: String,
+  },
+  methods: {
+    startDrag: (evt, id) => {
+      evt.dataTransfer.dropEffect = "move";
+      evt.dataTransfer.effectAllowed = "move";
+      evt.dataTransfer.setData("pid", id);
+    },
+  },
+};
+</script>
+
+<style>
+.circle {
+  cursor: pointer;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  top: 200px;
+  left: 200px;
+  border-radius: 99px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,42 @@
 <template>
-  <div class="home"></div>
+  <div
+    class="field"
+    @drop="onDrop($event)"
+    @dragover.prevent
+    @dragenter.prevent
+  >
+    <Player id="p1" />
+    <Player id="p2" />
+    <Player id="p3" />
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Player from "../components/game/Player";
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Player,
+  },
+  methods: {
+    onDrop: function(evt) {
+      const pid = evt.dataTransfer.getData("pid");
+      const item = document.getElementById(pid);
+      console.log(evt.clientY);
+      console.log(evt.clientX);
+      item.style.top = `${evt.clientY - 25}px`;
+      item.style.left = `${evt.clientX - 25}px`;
+    },
+  },
 };
 </script>
+
+<style>
+.field {
+  height: 800px;
+  width: 1000px;
+  border: 1px solid black;
+  background-color: gray;
+}
+</style>
