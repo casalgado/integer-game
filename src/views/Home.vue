330f6e8988ef391b0b19507206612972310228e9
casalgado <casalgado86@gmail.com>
2020-06-02 09:18:26 -0500

    fieldState obj

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,9 @@
     @dragover.prevent
     @dragenter.prevent
   >
-    <Player id="p1" />
-    <Player id="p2" />
-    <Player id="p3" />
+    <div v-for="obj in fieldState" :key="obj.id">
+      <Player :id="obj.id" />
+    </div>
   </div>
 </template>
 
@@ -19,15 +19,37 @@ export default {
   components: {
     Player,
   },
+  data() {
+    return {
+      fieldState: [
+        { id: "p1", x: 200, y: 300 },
+        { id: "p2", x: 300, y: 300 },
+        { id: "p3", x: 400, y: 300 },
+      ],
+    };
+  },
   methods: {
     onDrop: function(evt) {
       const pid = evt.dataTransfer.getData("pid");
-      const item = document.getElementById(pid);
-      console.log(evt.clientY);
-      console.log(evt.clientX);
-      item.style.top = `${evt.clientY - 25}px`;
-      item.style.left = `${evt.clientX - 25}px`;
+      for (let i = 0; i < this.fieldState.length; i++) {
+        let obj = this.fieldState[i];
+        if (obj.id == pid) {
+          obj.y = evt.clientY - 25;
+          obj.x = evt.clientX - 25;
+        }
+        this.draw();
+      }
     },
+    draw: function() {
+      for (let i = 0; i < this.fieldState.length; i++) {
+        const domObj = document.getElementById(this.fieldState[i].id);
+        domObj.style.top = `${this.fieldState[i].y}px`;
+        domObj.style.left = `${this.fieldState[i].x}px`;
+      }
+    },
+  },
+  mounted() {
+    this.draw();
   },
 };
 </script>
